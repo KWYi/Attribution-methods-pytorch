75f6bac4fdbaba1435fbdb72b3a3bf5823724a87
KWYi <48608835+KWYi@users.noreply.github.com>
2021-08-19 14:11:12 +0900

    Update Transformer.py

--- Transformer.py
+++ Transformer.py
@@ -7,13 +7,10 @@ class CustomImage(Dataset):
         super(CustomImage, self).__init__()
         self.transform = transforms.Compose([
                                             transforms.Resize(224),
-                                            # transforms.CenterCrop(224),
                                             transforms.ToTensor(),
                                             transforms.Normalize(
                                                 mean=[0.485, 0.456, 0.406],
                                                 std=[0.229, 0.224, 0.225]
-                                                # mean=[0.5, 0.5, 0.5],
-                                                # std=[0.5, 0.5, 0.5]
                                             )])
     def __call__(self, Input_img):
         Input_img = Input_img.convert('RGB')
